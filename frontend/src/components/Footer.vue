0b61afbd736f58873e7fd838fc909a9d030827f2
KwikKill <gabriel@blaisot.org>
2024-06-09 18:27:04 +0200

    Add login page

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col items-center justify-center border-t border-gray-300 bg-theme-nav lg:flex-row lg:gap-2 lg:pb-3">
+    <div>
+      © InsaLan, 2024
+    </div>
+    <div class="hidden text-gray-400 lg:block">
+      |
+    </div>
+    <div>
+      Distribué sous licence MIT
+    </div>
+    <div class="hidden  text-gray-400 lg:block">
+      |
+    </div>
+    <a
+      href="https://github.com/InsaLan/langate3000/"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="text-blue-500 hover:text-blue-700"
+    >
+      Sources
+    </a>
+  </div>
+</template>
