ac101a2741c8131eff4cf8c9e637ac982a0af4c4
KwikKill <gabriel@blaisot.org>
2024-06-30 17:32:11 +0200

    Complete models and stores

--- frontend/src/stores/user.store.ts
+++ frontend/src/stores/user.store.ts
@@ -11,6 +11,13 @@ export const useUserStore = defineStore('user', () => {
   const connectionTimestamp = ref(0);
   const router = useRouter();
 
+  function create_temp_password(): string {
+    // create a 4 digits password
+    const password = Math.floor(Math.random() * 9999).toString();
+    // Ensure that the password is a string of 4 digits
+    return password.padStart(4, '0');
+  }
+
   /*
   * Get a new csrf token from the server
   */
@@ -73,12 +80,89 @@ export const useUserStore = defineStore('user', () => {
     }
   }
 
+  async function create_user(data: User): Promise<void> {
+    await get_csrf();
+
+    try {
+      await axios.post('/user/users/', data, {
+        headers: {
+          'X-CSRFToken': csrf.value,
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      });
+    } catch (err) {
+      // TODO : display error message with a component
+      console.error((err as AxiosError).response?.data);
+    }
+  }
+
+  async function reset_password(id: number): Promise<string | undefined> {
+    await get_csrf();
+
+    const password = create_temp_password();
+
+    try {
+      await axios.post(`/user/change-password/${id}/`, { password }, {
+        headers: {
+          'X-CSRFToken': csrf.value,
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      });
+      return password;
+    } catch (err) {
+      // TODO : display error message with a component
+      console.error((err as AxiosError).response?.data);
+      return undefined;
+    }
+  }
+
+  async function delete_user(id: number): Promise<void> {
+    await get_csrf();
+
+    try {
+      await axios.delete(`/user/users/${id}/`, {
+        headers: {
+          'X-CSRFToken': csrf.value,
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      });
+    } catch (err) {
+      // TODO : display error message with a component
+      console.error((err as AxiosError).response?.data);
+    }
+  }
+
+  async function edit_user(id: number, data: User): Promise<void> {
+    await get_csrf();
+
+    try {
+      await axios.patch(`/user/users/${id}/`, data, {
+        headers: {
+          'X-CSRFToken': csrf.value,
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      });
+    } catch (err) {
+      // TODO : display error message with a component
+      console.error((err as AxiosError).response?.data);
+    }
+  }
+
   return {
     user,
     login,
     logout,
     get_csrf,
     handle_session_cookie_expiration,
+    create_user,
+    create_temp_password,
+    reset_password,
+    delete_user,
+    edit_user,
     isConnected,
     csrf,
     connectionTimestamp,
