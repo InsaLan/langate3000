ad1df62a111cd3519a4d6a75b7435c7c0221a0c4
KwikKill <gabriel@blaisot.org>
2024-10-09 23:31:27 +0200

    Mock nginx forward

--- backend/langate/user/tests.py
+++ backend/langate/user/tests.py
@@ -121,7 +121,13 @@ class UserEndToEndTestCase(TestCase):
         user.save()
 
         def send_valid_data(data):
-            request = self.client.post("/user/login/", data, format="json")
+            # Add HTTP_X_FORWARDED_FOR to simulate a request from a client
+            request = self.client.post(
+              "/user/login/",
+              data,
+              format="json",
+              HTTP_X_FORWARDED_FOR="127.0.0.1"
+            )
 
             self.assertEqual(request.status_code, 200)
             self.assertTrue("sessionid" in self.client.cookies)
@@ -164,7 +170,7 @@ class UserAPITestCase(TestCase):
         Test that the user can get his own information
         """
         self.client.force_authenticate(user=User.objects.get(username="randomplayer"))
-        request = self.client.get("/user/me/")
+        request = self.client.get("/user/me/", HTTP_X_FORWARDED_FOR="127.0.0.1")
 
         self.assertEqual(request.status_code, 200)
         self.assertEqual(request.data["username"], "randomplayer")
