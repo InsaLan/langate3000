8940b6e2595b7ca0ddbc703ac5284a0f2166df71
KwikKill <gabriel@blaisot.org>
2024-10-14 23:10:23 +0200

    Fix login notification error

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -17,16 +17,7 @@ const login_user = async () => {
     login_button.textContent = 'Connexion en cours...';
   }
 
-  const errors = await login(login_form.username, login_form.password);
-  // If there are errors, display them
-  if (errors) {
-    const login_error = document.getElementById('login_error');
-    if (login_error) {
-      login_error.classList.remove('hidden');
-      login_error.classList.add('block');
-      login_error.textContent = errors;
-    }
-  }
+  await login(login_form.username, login_form.password);
 
   // Change the login button back to normal
   if (login_button) {
@@ -49,9 +40,6 @@ const showPassword = ref(false);
       </div>
     </div>
     <form class="flex w-full flex-col gap-3 md:w-1/3">
-      <div id="login_error" class="hidden rounded-lg border border-red-500 p-4 text-red-500">
-        Erreur lors de la connexion, veuillez réessayer.
-      </div>
       <div class="flex flex-col gap-2">
         <label for="username">Nom d'utilisateur</label>
         <input id="username" v-model="login_form.username" type="text" name="username" required class="rounded-lg border border-black bg-theme-nav text-white"/>
