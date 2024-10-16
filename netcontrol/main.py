7930d99950812b9a3ae55b886eef95cdd6493b43
SkytAsul <skytasul@gmail.com>
2024-10-16 18:38:06 +0200

    Add nftables test

--- netcontrol/main.py
+++ netcontrol/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 import logging
-
-# TODO: check nftables access before initializing API
+from .nft import Nft
 
 logger = logging.getLogger('uvicorn.error')
 # for some reason, default loggers are not working with FastAPI
+
+logger.info("Checking that nftables is working...")
+nft = Nft(logger)
+nft.check_nftables()
 
 app = FastAPI()
 
---
+++ netcontrol/nft.py
@@ -0,0 +1,37 @@
+import nftables
+import json
+import logging
+
+class Nft:
+    """Class which interacts with the nftables backend
+    """
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
+        self.nft = nftables.Nftables()
+        self.nft.set_json_output(True)
+
+    def check_nftables(self):
+        data = self._execute_nft_cmd("list ruleset")
+        metainfo = data[0]["metainfo"]
+        self.logger.info(f"Found running nftables version {metainfo['version']} with {len(data)} ruleset entries.")
+    
+    def _execute_nft_cmd(self, cmd: str) -> dict:
+        """Executes an nft command, handles the exception properly and returns an object
+
+        Args:
+            cmd (str): string representation of the command
+
+        Raises:
+            NftablesException: if the command returned an exception
+
+        Returns:
+            dict: parsed JSON output
+        """
+        output: str
+        rc, output, error = self.nft.cmd("list ruleset")
+        if rc != 0 or (error is not None and error != ""):
+            raise NftablesException(rc, error)
+        return json.loads(output)["nftables"]
+
+class NftablesException(Exception):
+    pass
