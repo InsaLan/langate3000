dbe153a3e3e16a5ca1b5ee297c27f3aa9a6ec001
Ecnama <amance@famille-graindorge.fr>
2024-10-16 20:31:00 +0200

    add get_mac and get_ip methods

--- netcontrol/main.py
+++ netcontrol/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 import logging
 from .nft import Nft
+from .arp import Arp
 
 logger = logging.getLogger('uvicorn.error')
 # for some reason, default loggers are not working with FastAPI
 
 logger.info("Checking that nftables is working...")
 nft = Nft(logger)
+arp = Arp(logger)
 nft.check_nftables()
 
 app = FastAPI()
@@ -14,3 +16,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return "netcontrol is running"
+
+@app.get("/get_mac/{ip_address}")
+def get_mac(ip: str):
+    return arp.get_mac(ip)
+
+@app.post("/connect_user/")
+def connect_user(mac: str, mark: int, name: str):
+    return nft.connect_user(mac, mark, name)
+
+@app.delete("/disconnect_user/{mac}")
+def delete_user(mac: str):
+    return nft.delete_user(mac)
+
+@app.put("/set_mark/")
+def set_mark(mac: str, mark: int):
+    return nft.set_mark(mac, mark)
