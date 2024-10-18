2689f75cb4f02c2afda233b7cc7965baba26f47d
Ecnama <amance@famille-graindorge.fr>
2024-10-18 20:09:58 +0200

    Add Netcontrol class with request methods

--- backend/langate/modules/netcontrol.py
+++ backend/langate/modules/netcontrol.py
@@ -1,52 +1,114 @@
-import  socket, struct
-import pickle
-import threading
-from ..settings import NETCONTROL_SOCKET_FILE
+import requests
+import subprocess
+import logging
 
-lock = threading.Lock()
+GET_REQUESTS = ["get_mac", "get_ip"]
+POST_REQUESTS = ["connect_user"]
+DELETE_REQUESTS = ["disconnect_user"]
+PUT_REQUESTS = ["set_mark"]
 
-class NetworkDaemonError(RuntimeError):
-    """ every error originating from netcontrol raise this exception  """
+class RequestError(Exception):
+    """
+    Exception raised when a request to the netcontrol API fails.
+    """
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
-def _send(sock, data):
-    pack = struct.pack('>I', len(data)) + data
-    sock.sendall(pack)
+class NetControl:
+    """
+    Class which interacts with the netcontrol API.
+    """
+    def __init__(self):
+        """
+        Initialize HOST_IP to the docker's default route and check the connection with the netcontrol API.
+        """
+        self.HOST_IP = subprocess.run(["/sbin/ip", "route"], capture_output=True).stdout.decode("utf-8").split()[2]
+        self.logger = logging.getLogger(__name__)
 
-def _recv_bytes(sock, size):
-    data = b''
-    while len(data) < size:
-        r = sock.recv(size - len(data))
-        if not r:
-            return None
-        data += r
-    return data
+        try:
+            self.logger.info("Checking connection with the netcontrol API...")
+            if requests.get(f"http://{self.HOST_IP}:6784/") != "netcontrol is running":
+                raise RequestError("Netcontrol is not running.")
+        except requests.exceptions.ConnectionError:
+            self.logger.info("Could not connect to the netcontrol API.")
+        except RequestError as e:
+            self.logger.info(e.message)
 
-def _recv(sock):
-    data_length_r = _recv_bytes(sock, 4)
+    def request(self, endpoint='', args={}):
+        """
+        Make a given request to the netcontrol API.
+        """
+        # Construct the data to be sent in the request
+        if len(args) == 1:
+            data = '/'+args[0]
+        elif len(args) > 1:
+            data = '?' + "&".join([f"{key}={value}" for key, value in args.items()])
+        else:
+            data = ''
 
-    if not data_length_r:
-        return None
+        # Make the request
+        try:
+            # Check the type of request
+            if endpoint in GET_REQUESTS:
+                response = requests.get(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
+            elif endpoint in POST_REQUESTS:
+                response = requests.post(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
+            elif endpoint in DELETE_REQUESTS:
+                response = requests.delete(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
+            elif endpoint in PUT_REQUESTS:
+                response = requests.put(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
+            
+            # Check for errors in the response
+            if "error" in response.json().keys():
+                raise RequestError(response.json()["error"])
+            return response.json()
+        
+        # Handle exceptions
+        except requests.exceptions.ConnectionError:
+            self.logger.info("Could not connect to the netcontrol API.")
+        except RequestError as e:
+            self.logger.info(e.message)
 
-    data_length = struct.unpack('>I', data_length_r)[0]
-    return _recv_bytes(sock, data_length)
+    def check_api(self):
+        """
+        Check if the netcontrol API is running.
+        """
+        self.logger.info("Checking connection with the netcontrol API...")
+        return self.request()
 
-
-def communicate(payload):
-    with lock:
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-            sock.connect(NETCONTROL_SOCKET_FILE)
-            r = pickle.dumps(payload)
-            _send(sock, r)
-
-            response_r = _recv(sock)
-            response = pickle.loads(response_r)
-
-            if response["success"]:
-                return response
-
-            else:
-                raise NetworkDaemonError(response["message"])
-
-def query(q, opts = {}):
-    b = { "query": q }
-    return communicate({ **b, **opts })
+    def get_mac(self, ip: str):
+        """
+        Get the MAC address of the device with the given IP address.
+        """
+        self.logger.info(f"Getting MAC address of {ip}...")
+        return self.request("get_mac", {"ip": ip})["mac"]
+    
+    def get_ip(self, mac: str):
+        """
+        Get the IP address of the device with the given MAC address.
+        """
+        self.logger.info(f"Getting IP address of {mac}...")
+        return self.request("get_ip", {"mac": mac})["ip"]
+    
+    def connect_user(self, mac: str, mark: int, name: str):
+        """
+        Connect the user with the given MAC address.
+        """
+        self.logger.info(f"Connecting user with MAC address {mac} ({name})...")
+        return self.request("connect_user", {"mac": mac, "mark": mark, "name": name})
+    
+    def disconnect_user(self, mac: str):
+        """
+        Disconnect the user with the given MAC address.
+        """
+        self.logger.info(f"Disconnecting user with MAC address {mac}...")
+        return self.request("disconnect_user", {"mac": mac})
+    
+    def set_mark(self, mac: str, mark: int):
+        """
+        Set the mark of the user with the given MAC address.
+        """
+        self.logger.info(f"Setting mark of user with MAC address {mac} to {mark}...")
+        return self.request("set_mark", {"mac": mac, "mark": mark})
+    
