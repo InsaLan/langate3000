9c400c32c4896849aa45c608e779f83ca6f26956
Ecnama <amance@famille-graindorge.fr>
2024-10-20 01:19:52 +0200

    Use HTTPExceptions

--- backend/langate/modules/netcontrol.py
+++ backend/langate/modules/netcontrol.py
@@ -1,19 +1,12 @@
 import requests
 import subprocess
 import logging
+from fastapi import HTTPException
 
 GET_REQUESTS = ["get_mac", "get_ip"]
 POST_REQUESTS = ["connect_user"]
 DELETE_REQUESTS = ["disconnect_user"]
 PUT_REQUESTS = ["set_mark"]
-
-class RequestError(Exception):
-    """
-    Exception raised when a request to the netcontrol API fails.
-    """
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
 
 class Netcontrol:
     """
@@ -21,19 +14,24 @@ class Netcontrol:
     """
     def __init__(self):
         """
-        Initialize HOST_IP to the docker's default route and check the connection with the netcontrol API.
+        Initialize HOST_IP to the docker's default route, set up REQUEST_URL and check the connection with the netcontrol API.
         """
         self.HOST_IP = subprocess.run(["/sbin/ip", "route"], capture_output=True).stdout.decode("utf-8").split()[2]
+        self.REQUEST_URL = f"http://{self.HOST_IP}:6784/"
+
         self.logger = logging.getLogger(__name__)
 
+        # Check the connection with the netcontrol API
         try:
-            self.logger.info("Checking connection with the netcontrol API...")
-            if requests.get(f"http://{self.HOST_IP}:6784/") != "netcontrol is running":
-                raise RequestError("Netcontrol is not running.")
-        except requests.exceptions.ConnectionError:
-            self.logger.info("Could not connect to the netcontrol API.")
-        except RequestError as e:
-            self.logger.info(e.message)
+            try:
+                self.logger.info("Checking connection with the netcontrol API...")
+                if requests.get(self.REQUEST_URL) != "netcontrol is running":
+                    raise HTTPException(status_code=404, detail="Netcontrol is not running.")
+                
+            except requests.exceptions.ConnectionError:
+                raise HTTPException(status_code=408, detail="Could not connect to the netcontrol API.")
+        except HTTPException as e:
+            self.logger.info(e.detail)
 
     def request(self, endpoint='', args={}):
         """
@@ -49,28 +47,24 @@ class Netcontrol:
 
         # Make the request
         try:
-            # Check the type of request
-            if endpoint in GET_REQUESTS:
-                response = requests.get(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
-            elif endpoint in POST_REQUESTS:
-                response = requests.post(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
-            elif endpoint in DELETE_REQUESTS:
-                response = requests.delete(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
-            elif endpoint in PUT_REQUESTS:
-                response = requests.put(f"http://{self.HOST_IP}:6784/{endpoint}{data}")
+            try:
+                # Check the type of request
+                if endpoint in GET_REQUESTS:
+                    response = requests.get(self.REQUEST_URL + endpoint + data)
+                elif endpoint in POST_REQUESTS:
+                    response = requests.post(self.REQUEST_URL + endpoint + data)
+                elif endpoint in DELETE_REQUESTS:
+                    response = requests.delete(self.REQUEST_URL + endpoint + data)
+                elif endpoint in PUT_REQUESTS:
+                    response = requests.put(self.REQUEST_URL + endpoint + data)
+                
+                response.raise_for_status()
+                return response.json()
             
-            # Check for errors in the response
-            if response.json()["error"]:
-                raise RequestError(response.json()["error"])
-            return response.json()
-        
-        # Handle exceptions
-        except requests.exceptions.ConnectionError:
-            self.logger.info("Could not connect to the netcontrol API.")
-            return {"error": "Could not connect to the netcontrol API."}
-        except RequestError as e:
-            self.logger.info(e.message)
-            return response.json()
+            except requests.exceptions.ConnectionError:
+                raise HTTPException(status_code=408, detail="Could not connect to the netcontrol API.")
+        except HTTPException as e:
+            self.logger.info(e.detail) # Handle HTTP exception (TODO)
 
     def check_api(self):
         """
