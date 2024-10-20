a5f49bce44f6cac1a0c7d27f1b88fc2956c538c8
Awing <awingding.win@gmail.com>
2024-10-20 15:49:43 +0200

    extract code to another file, change call to wrong program

--- netcontrol/nft.py
+++ netcontrol/nft.py
@@ -127,59 +127,5 @@ class Nft:
 
         self.logger.info(f"Device {mac} disconnected")
 
-    def _add_vpn(self, mark: int, vpn: str) -> None:
-        """Adds a VPN to the nftables rule
-
-        Args:
-            mark (int): mark value
-            vpn (str): VPN routing table name
-        """
-        try:
-            self._execute_nft_cmd(f"add rule insalan netcontrol-forward fwmark {mark} lookup {vpn}")
-        except NftablesException as e:
-            self.logger.error(f"Failed to add VPN {vpn} with mark {mark}: {e}")
-
-    def _remove_vpn(self, mark: int, vpn: str) -> None:
-        """Removes a VPN from the nftables rule
-
-        Args:
-            mark (int): mark value
-            vpn (str): VPN routing table name
-        """
-        try:
-            self._execute_nft_cmd(f"delete rule insalan netcontrol-forward fwmark {mark} lookup {vpn}")
-        except NftablesException as e:
-            self.logger.error(f"Failed to remove VPN {vpn} with mark {mark}: {e}")
-
-    def _parse_etc_hosts(self) -> tuple:
-        """Parses /etc/hosts and return all vpn entries
-
-        Returns:
-            tuple: n-uple of VPN entries
-        """
-        with open("/etc/hosts", "r") as f:
-            lines = f.readlines()
-        vpns = []
-        for line in lines:
-          if match := re.match(r".+(vpn\d+).+", line):
-                vpns.append(match.group())
-        return tuple(vpns)
-
-    def initialize_vpn(self) -> None:
-        """Initializes the VPN routing tables
-        """
-        vpns = self._parse_etc_hosts()
-        for i, vpn in enumerate(vpns):
-            self._add_vpn(100+i, vpn)
-        self.logger.info(f"Initialized {len(vpns)} VPN routing tables.")
-
-    def remove_vpn(self) -> None:
-        """Removes the VPN routing tables
-        """
-        vpns = self._parse_etc_hosts()
-        for i, vpn in enumerate(vpns):
-            self._remove_vpn(100+i, vpn)
-        self.logger.info(f"Removed {len(vpns)} VPN routing tables.")
-
 class NftablesException(Exception):
     pass
