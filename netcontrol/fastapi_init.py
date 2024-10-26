daaa7a720beff39e67224ef0b21a52016cd826c3
Ecnama <amance@famille-graindorge.fr>
2024-10-26 16:29:13 +0200

    Swith to bash

--- netcontrol/fastapi_init.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def get_ip():
-    """
-    Function that returns the IP of the docker bridge.
-    """
-    return subprocess.run(["ip", "-br", "address", "show", "dev", "docker0"], capture_output=True).stdout.decode().split()[2][:-3]
-
-subprocess.run(["fastapi", "run", "main.py", "--host", get_ip(), "--port", "6784"])
