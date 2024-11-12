ac3aed4754863518b413a3035d5bc1b180623be3
Ecnama <amance@famille-graindorge.fr>
2024-11-12 09:59:39 +0100

    Change to a nft rule

--- netcontrol/fastapi_init.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-# Get the IP address of the docker bridge
-IP="$(ip addr show docker0 | awk '/inet / {print $2}' | cut -d'/' -f1)"
-# Run the FastAPI server
-fastapi run main.py --host $IP --port 6784
